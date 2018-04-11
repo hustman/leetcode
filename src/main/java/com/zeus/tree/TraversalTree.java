a5690b48d023e76f5f82b9c4e366c0f947f625d7
xingbo.xu <xingbo.xu@qunar.com>
2018-04-12 00:39:32 +0800

    遍历二叉树的各种方式

--- src/main/java/com/zeus/tree/TraversalTree.java
+++ src/main/java/com/zeus/tree/TraversalTree.java
@@ -1,8 +1,38 @@
 package com.zeus.tree;
 
+import java.util.List;
+
 /**
  * @author xuxingbo
  * @Date 2018/4/12
+ * 各种方式遍历二叉树
  */
 public class TraversalTree {
+    /**
+     * 先序遍历
+     */
+    public List<Integer> preOrder(TreeNode root){
+        return null;
+    }
+    /**
+     * 中序遍历
+     */
+    public List<Integer> inOrder(TreeNode root){
+        return null;
+    }
+    
+    /**
+     * 后序遍历
+     */
+    public List<Integer> postOrder(TreeNode root){
+        return null;
+    }
+    
+    /**
+     * 层序遍历
+     */
+    public List<Integer> levelOrder(TreeNode node){
+        return null;
+    }
+    
 }
--- src/main/java/com/zeus/tree/TreeNode.java
+++ src/main/java/com/zeus/tree/TreeNode.java
@@ -5,4 +5,17 @@ package com.zeus.tree;
  * @Date 2018/4/12
  */
 public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int value;
+    
+    public TreeNode(int value) {
+        this.value = value;
+    }
+    
+    public TreeNode(int value,TreeNode left, TreeNode right) {
+        this.left = left;
+        this.right = right;
+        this.value = value;
+    }
 }
